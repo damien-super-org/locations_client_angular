1b39b0e184160dc8b8a4641b0db4c5c995c3f565
Aziz <aziz@humanitec.com>
2019-04-03 17:54:40 +0200

    update location model

--- src/lib/state/location.model.ts
+++ src/lib/state/location.model.ts
@@ -1,4 +1,25 @@
 export interface Location {
   id?: number;
+  uuid: string;
+  organisation_uuid: string;
+  name: string;
+  address_line1: string;
+  address_line2: string;
+  address_line3: string;
+  address_line4: string;
+  postcode: string;
+  city: string;
+  country: string;
+  administrative_level1: string;
+  administrative_level2: string;
+  administrative_level3: string;
+  administrative_level4: string;
+  latitude: string;
+  longitude: string;
+  notes: string;
+  create_date: string;
+  edit_date: string;
+  workflowlevel2_uuid: string;
+  profiletype: string;
   // add other properties here
 }
