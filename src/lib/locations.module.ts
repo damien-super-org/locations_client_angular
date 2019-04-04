5c4d35ee7e6a7e677705201615a1a44d7c212a11
Aziz <aziz@humanitec.com>
2019-04-04 17:53:44 +0200

    minor fix in ngmodule

--- src/lib/locations.module.ts
+++ src/lib/locations.module.ts
@@ -3,16 +3,12 @@ import { LocationsComponent } from './locations.component';
 import { MidgardSharedTranslationModule } from '@midgard/modules/translation/translation.shared.module';
 import { LocationsRoutingModule } from './locations-routing.module';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
-import { LazyloadingHelper } from '../../../../midgard/helpers/lazyloading.helper';
 
 @NgModule({
   imports: [
     MidgardSharedTranslationModule,
     LocationsRoutingModule,
     LeafletModule.forRoot()
-  ],
-  providers: [
-    LazyloadingHelper
   ],
   declarations: [LocationsComponent],
   exports: [LocationsComponent]
